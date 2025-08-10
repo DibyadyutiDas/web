c26fc2c403a381777be9e47324130df1517672c2
Dibyadyuti Das <dibyadyutidas0@gmail.com>
2025-08-10 10:45:02 +0530

    Delete javascript directory

--- javascript/questions/factorial.js
+++
@@ -1,9 +0,0 @@
-//? Wap to find the factorial of a given number
-// For example, the factorial of 5 is 120 (5 * 4 * 3 * 2 * 1)
-
-let num = prompt('Enter a number:');
-let factorial = 1;
-for (let i = 1; i <= num; i++) {
-    factorial *= i;
-}
-alert("The factorial of " + num + " is " + factorial);
